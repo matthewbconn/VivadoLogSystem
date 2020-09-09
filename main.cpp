aa17f253881bebc3c41747f5616c8cfd907a6eb3
matthewbconn <46605086+matthewbconn@users.noreply.github.com>
2020-09-09 14:43:18 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "lognum.h"
 #include "ConversionEngine.cpp"
 #define TESTCASES 10000
+#define LARGEFRAC 0.1
 
 using namespace std;
 
@@ -16,8 +17,11 @@ double convertToDouble(lognum L);
 void printDemoCases();
 void MACdemo();
 void runTests();
+void runTestsBigNums();
 void runTestsSmallNums();
 void runTestsMixedNums();
+template<typename T>
+void printErrors(T addErrors, T addSum, T multErrors, T multSum, int largeCt);
 
 int main() {
     cout << "Hello, World!" << "\n" << endl;
@@ -26,9 +30,10 @@ int main() {
     MACdemo();
 
     srand(time(NULL));
+    runTestsMixedNums();
+    runTestsBigNums();
     runTests();
     runTestsSmallNums();
-    runTestsMixedNums();
 
     return 0;
 }
@@ -44,11 +49,11 @@ string sign(bool b) {
  * */
 int convertToInt(lognum L) {
     double temp = L.getLogval();
-    int d = pow(2,L.getLogval());
+    double d = pow(2,L.getLogval());
     if (L.getSignBit()) {
         return d;
     }
-    return (-1*d);
+    return (int)(-1*d);
 }
 
 /*
@@ -85,6 +90,9 @@ void printDemoCases() {
 
     lognum ZplusY(lognum::addReals(Z,W));
     lognum ZtimesY(lognum::multiplyReals(Z,W));
+    cout << "v = " << v << endl;
+    cout << "w = " << w << endl;
+    cout << "z = " << z << endl;
     cout << "Z + W to Integer computed as: " << convertToInt(ZplusY) << endl;
     cout << "Z + W to Float computed as: " << convertToDouble(ZplusY) << endl;
     cout << "Z * W to Integer computed as: " << convertToInt(ZtimesY) << endl;
@@ -97,10 +105,12 @@ void printDemoCases() {
 }
 
 void MACdemo() {
-    int m(1); lognum M(toLogNum(m));
-    int n(0); lognum N(toLogNum(n));
+    int m(2); lognum M(toLogNum(m));
+    int n(2); lognum N(toLogNum(n));
+    cout << "\nInitial value: " << convertToDouble(N) << "\n\n";
     for (int i = 0; i < 10; ++i) {
         N.MAC(M,M);
+        cout << "After iteration " << i << " value is " << convertToDouble(N) << endl;
     }
     cout << "\nAfter 10 multiply accumulate ops: " << convertToDouble(N) << "\n" << endl;
 }
@@ -112,25 +122,32 @@ void runTests() {
     double multErrorMargin(0);
     int largeMulErrorCt(0);
 
+    cout << "runTests\n";
+
     // Currently allotting 6 signed bits for integer
     // log2(|x|) can range from 2^-5 ( = -32)  to just below 2^5 - 1 ( = 31 - eps)
     // Corresponding x ranges from  just above 0 to just below 2^31 ~ 10^9
     for (int i = 0; i < TESTCASES; ++i) {
-        int r1((rand() % 1024*1024) - 127);
-        int r2((rand() % 1024*1024) + 0);
+        int r1((rand() % 1024*32));
+        int r2((rand() % 1024*32));
         lognum R1(toLogNum(r1)), R2(toLogNum(r2));
-        // notice this line right here: you are getting the next lowest power of 2...wonder why
+
+        // ensure conversion back and forth works right
         int r1test(convertToInt(R1)),r2test(convertToInt(R2));
-        int expSum = r2 + r1;
-        int expProd = r2 * r1;
+
+        long long expSum = r2 + r1;
         double calcSum = convertToDouble(lognum::addReals(R1,R2));
-        double calcProd = convertToDouble(lognum::multiplyReals(R1,R2));
         addErrors += abs(calcSum - 1.0*expSum);
         double addMargin = abs(expSum) < abs(calcSum) ? abs(expSum) : abs(calcSum);
         addErrorMargin += addMargin;
+
+        double expProd = 1.0*r2 * r1;
+
+        // why is calcProd not changing at all?! calcSum changes
+        double calcProd(convertToDouble(lognum::multiplyReals(R1,R2)));
+
         multErrors += abs(calcProd - 1.0*expProd);
-        if (abs(calcProd - 1.0 * expProd) > 0.1 * abs(expProd)) {
-//            cout << "Large error when multiplying " << r1 << " to " << r2 << endl;
+        if (abs(calcProd - expProd) > LARGEFRAC * abs(expProd)) {
             ++largeMulErrorCt;
         }
         double multMargin = abs(expProd) < abs(calcProd) ? abs(expProd) : abs(calcProd);
@@ -138,11 +155,7 @@ void runTests() {
     }
 
     cout << "Resuts for both numbers big: " << endl;
-    cout << "Avg. error per addition test\t" << addErrors/TESTCASES << endl;
-    printf("Relative to range\t\t %2.4f",((100.0*addErrors)/(addErrorMargin*TESTCASES))); cout << " %\n";
-    cout << "Avg. error per mult. test\t" << multErrors/TESTCASES << endl;
-    printf("Relative to range\t\t %2.4f",((100.0*multErrors)/(multErrorMargin*TESTCASES))); cout << " %\n";
-    cout << "Total large errors: " << largeMulErrorCt << "\n\n";
+    printErrors(addErrors,addErrorMargin,multErrors,multErrorMargin,largeMulErrorCt);
 }
 
 void runTestsSmallNums() {
@@ -159,7 +172,8 @@ void runTestsSmallNums() {
         double r1(1.0*((rand() % 1000))/1000); if (i%2) {r1 *= -1;}
         double r2(1.0*((rand() % 1000))/1000); if (i%4) {r2 *= -1;}
         lognum R1(toLogNum(r1)), R2(toLogNum(r2));
-        // notice this line right here: you are getting the next lowest power of 2...wonder why
+
+        // ensure conversion back and forth works fine
         double r1test(convertToDouble(R1)),r2test(convertToDouble(R2));
         double expSum = r2 + r1;
         double expProd = r2 * r1;
@@ -169,8 +183,7 @@ void runTestsSmallNums() {
         double addMargin = abs(expSum) < abs(calcSum) ? abs(expSum) : abs(calcSum);
         addErrorMargin += addMargin;
         multErrors += abs(calcProd - 1.0*expProd);
-        if (abs(calcProd - 1.0 * expProd) > 0.1 * abs(expProd)) {
-//            cout << "Large error when multiplying " << r1 << " to " << r2 << endl;
+        if (abs(calcProd - 1.0 * expProd) > LARGEFRAC * abs(expProd)) {
             ++largeMulErrorCt;
         }
         double multMargin = abs(expProd) < abs(calcProd) ? abs(expProd) : abs(calcProd);
@@ -178,11 +191,7 @@ void runTestsSmallNums() {
     }
 
     cout << "Results for both numbers small:" << endl;
-    cout << "Avg. error per addition test\t" << addErrors/TESTCASES << endl;
-    printf("Relative to range\t\t %2.9f",((100.0*addErrors)/(addErrorMargin*TESTCASES))); cout << " %\n";
-    cout << "Avg. error per mult test\t" << multErrors/TESTCASES << endl;
-    printf("Relative to range\t\t %2.9f",((100.0*multErrors)/(multErrorMargin*TESTCASES))); cout << " %\n";
-    cout << "Total large errors: " << largeMulErrorCt << "\n\n";
+    printErrors(addErrors,addErrorMargin,multErrors,multErrorMargin,largeMulErrorCt);
 }
 
 void runTestsMixedNums() {
@@ -199,7 +208,8 @@ void runTestsMixedNums() {
         double r1(1.0*((rand() % 1000))/1000); if (i%2) {r1 *= -1;}
         double r2((rand() % 1024*1024) + 0);
         lognum R1(toLogNum(r1)), R2(toLogNum(r2));
-        // notice this line right here: you are getting the next lowest power of 2...wonder why
+
+        // ensure conversion back and forth works fine
         double r1test(convertToDouble(R1)),r2test(convertToDouble(R2));
         double expSum = r2 + r1;
         double expProd = r2 * r1;
@@ -209,8 +219,7 @@ void runTestsMixedNums() {
         double addMargin = abs(expSum) < abs(calcSum) ? abs(expSum) : abs(calcSum);
         addErrorMargin += addMargin;
         multErrors += abs(calcProd - 1.0*expProd);
-        if (abs(calcProd - 1.0 * expProd) > 0.1 * abs(expProd)) {
-//            cout << "Large error when multiplying " << r1 << " to " << r2 << endl;
+        if (abs(calcProd - 1.0 * expProd) > LARGEFRAC * abs(expProd)) {
             ++largeMulErrorCt;
         }
         double multMargin = abs(expProd) < abs(calcProd) ? abs(expProd) : abs(calcProd);
@@ -218,9 +227,56 @@ void runTestsMixedNums() {
     }
 
     cout << "Results for one number small one number big:" << endl;
+    printErrors(addErrors,addErrorMargin,multErrors,multErrorMargin,largeMulErrorCt);
+}
+
+void runTestsBigNums() {
+    double addErrors(0);
+    double addErrorMargin(0);
+    double multErrors(0);
+    double multErrorMargin(0);
+    int largeMulErrorCt(0);
+
+    cout << "runTestsBigNums\n";
+    // Currently allotting 6 signed bits for integer
+    // log2(|x|) can range from 2^-5 ( = -32)  to just below 2^5 - 1 ( = 31 - eps)
+    // Corresponding x ranges from  just above 0 to just below 2^31 ~ 10^9
+    for (int i = 0; i < TESTCASES; ++i) {
+        int r1((rand() % 1024*32));
+        int r2((rand() % 1024*32));
+        lognum R1(toLogNum(r1)), R2(toLogNum(r2));
+
+        // ensure conversion back and forth works right
+        int r1test(convertToInt(R1)),r2test(convertToInt(R2));
+
+        int expSum = r2 + r1;
+        double calcSum = convertToDouble(lognum::addReals(R1,R2));
+        addErrors += abs(calcSum - 1.0*expSum);
+        double addMargin = abs(expSum) < abs(calcSum) ? abs(expSum) : abs(calcSum);
+        addErrorMargin += addMargin;
+
+        double expProd = 1.0*r2 * r1;
+
+        // why is calcProd not changing at all?! calcSum changes
+        double calcProd = convertToDouble(lognum::multiplyReals(R1,R2));
+
+        multErrors += abs(calcProd - 1.0*expProd);
+        if (abs(calcProd - expProd) > LARGEFRAC * abs(expProd)) {
+            ++largeMulErrorCt;
+        }
+        double multMargin = abs(expProd) < abs(calcProd) ? abs(expProd) : abs(calcProd);
+        multErrorMargin += multMargin;
+    }
+
+    cout << "Resuts for both numbers big: " << endl;
+    printErrors(addErrors,addErrorMargin,multErrors,multErrorMargin,largeMulErrorCt);
+}
+
+template<typename T>
+void printErrors(T addErrors, T addSum, T multErrors, T multSum, int largeCt) {
     cout << "Avg. error per addition test\t" << addErrors/TESTCASES << endl;
-    printf("Relative to range\t\t %2.4f",((100.0*addErrors)/(addErrorMargin*TESTCASES))); cout << " %\n";
-    cout << "Avg. error per mult test\t" << multErrors/TESTCASES << endl;
-    printf("Relative to range\t\t %2.4f",((100.0*multErrors)/(multErrorMargin*TESTCASES))); cout << " %\n";
-    cout << "Total large errors: " << largeMulErrorCt << "\n\n";
+    printf("Relative to range\t\t %2.9f",((100.0*addErrors)/(addSum*TESTCASES))); cout << " %\n";
+    cout << "Avg. error per mult. test\t" << multErrors/TESTCASES << endl;
+    printf("Relative to range\t\t %2.9f",((100.0*multErrors)/(multSum*TESTCASES))); cout << " %\n";
+    printf("Percent large errors\t\t %2.4f",((100.0*largeCt)/(TESTCASES))); cout << " %\n\n";
 }
