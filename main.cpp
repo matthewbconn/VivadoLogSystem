7d8b5cb948127f3477876a4937f4648259e6f6fb
matthewbconn <46605086+matthewbconn@users.noreply.github.com>
2020-09-23 03:01:42 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 string sign(bool b);
 int convertToInt(lognum L);
 double convertToDouble(lognum L);
+void runMSETestforDeltaComparison();
 void printDemoCases();
 void MACdemo();
 void runTests();
@@ -21,6 +22,9 @@ void runTestsBigNums();
 void runTestsSmallNums();
 void runTestsMixedNums();
 void runMSETest();
+void runMSE_Delta_Test_Procedure();
+void printMSE_Errors(int largeErrorCt,double MSE_MAC, double MSE_ACC, double MSE_MUL);
+void printMSE_Errors_Short(int largeErrorCt, double MSE);
 
 template<typename T>
 void printErrors(T addErrors, T addSum, T multErrors, T multSum, int largeCt);
@@ -28,7 +32,8 @@ void printMinMax();
 
 int main() {
     srand(time(NULL));
-    runMSETest();
+
+    runMSE_Delta_Test_Procedure();
 
 /*
     printDemoCases();
@@ -38,7 +43,8 @@ int main() {
     // deprecated? //runTestsBigNums();
 
     MACdemo();
-
+    runMSETest();
+    // deprecated runMSETestforDeltaComparison()
     printMinMax();
 */
 
@@ -55,7 +61,6 @@ string sign(bool b) {
  * Output: the corresponding real value, as an int
  * */
 int convertToInt(lognum L) {
-    double temp = L.getLogval();
     double d = pow(2,L.getLogval());
     if (L.getSignBit()) {
         return d;
@@ -227,6 +232,9 @@ void runTestsMixedNums() {
     printErrors(addErrors,addErrorMargin,multErrors,multErrorMargin,largeMulErrorCt);
 }
 
+/*
+ * Functional but deprecated
+ * */
 void runTestsBigNums() {
     double addErrors(0), addErrorMargin(0), multErrors(0), multErrorMargin(0);
     int largeMulErrorCt(0);
@@ -269,7 +277,7 @@ void runTestsBigNums() {
 void runMSETest() {
     double MSE_MUL(0.0),MSE(0.0),MSE_ACC_REF(0.0); int largeErrorCt(0);
 
-    cout << "MSE calculated on 10k MAC operations with floating point inputs on (-2^15, 2^15)\n\n";
+    cout << "MSE calculated on 10k MAC operations with floating point inputs on (-2^15, 2^15)\n";
 
     // Currently allotting 6 signed bits for integer
     // log2(|x|) can range from 2^-5 ( = -32)  to just below 2^5 - 1 ( = 31 - eps)
@@ -324,14 +332,232 @@ void runMSETest() {
     double RMSD_MUL = sqrt(MSE_MUL/TESTCASES);
     double RMSD_ACC_REF = sqrt(MSE_ACC_REF/TESTCASES);
 
-    printf("Test cases off by > 10%: \t\t\t %2.6f",((100.0*largeErrorCt)/(TESTCASES))); cout << " %\n";
-    printf("MSE on 10k MAC operatons: \t\t\t%.2f",(MSE));
-    printf("\nMSE from reference ADD ops was : \t\t%.2f",(MSE_ACC_REF));
-    printf("\nMSE from the MUL ops was : \t\t\t%.2f",(MSE_MUL));
+    printMSE_Errors(largeErrorCt,MSE,MSE_ACC_REF,MSE_MUL);
+}
 
-    printf("\n\nRMSD on 10k MAC operations: \t\t\t%.5f",(RMSD));
-    printf("\nRMSD of 10k reference ADD ops operations: \t%.5f",(RMSD_ACC_REF));
-    printf("\nRMSD of 10k MUL operations: \t\t\t%.5f",(RMSD_MUL));
+/*
+ * This function was deprecated on 9/21/20
+ * Do not use it.  For explanation see email to pabeerel@usc.edu on 9/23
+ * which was a correction to the email of 9/22
+ * */
+void runMSETestforDeltaComparison() {
+    cout << "STOP.\n";
+    cout << "Entering a deprecated function.  Stop and find corrected one" << endl;
+    int unexpectedErrorCt(0);
+    double P_MSE_MUL(0.0),P_MSE(0.0),P_MSE_ACC_REF(0.0); int P_largeErrorCt(0);
+    double M_MSE_MUL(0.0),M_MSE(0.0),M_MSE_ACC_REF(0.0); int M_largeErrorCt(0);
+
+    cout << "Begin delta function testing\n";
+    printf("MSE calculated on %d MAC operations with floating point inputs on (-2^15, 2^15)\n\n", (TESTCASES));
+
+    // Currently allotting 6 signed bits for integer
+    // log2(|x|) can range from 2^-5 ( = -32)  to just below 2^5 - 1 ( = 31 - eps)
+    // Corresponding x ranges from  just above 0 to just below 2^31 ~ 10^9
+    for (int i = 0; i < TESTCASES; ++i) {
+
+/*      // not used so we can emphasize cases where d is small and errors are worse
+
+        // get integer bases, range up to 2^15
+        int base_num = 1024*32;
+        int32_t r1_int((rand() % (base_num)));
+        int32_t r2_int((rand() % (base_num)));
+        int32_t r3_int((rand() % (base_num)));
+*/
+
+        // get floating point values for A + B*C
+        double r1 = (double)rand()/RAND_MAX; double r2 = (double)rand()/RAND_MAX; double r3 = (double)rand()/RAND_MAX;
+        double pr1(r1),pr2(r2),pr3(r3),mr1(r1),mr2(r2),mr3(r3);
+
+        // make half of the signbit pairs (pos,pos) and other half (neg,neg) for delta plus
+        if (i%2) {pr2*= -1.0; pr3*= -1.0;}
+
+        // make all of the signbit pairs (neg, pos) for delta minus
+        mr2 *= -1.0;
+
+        // log forms
+        lognum PR1(toLogNum(pr1)), PR2(toLogNum(pr2)),PR3(toLogNum(pr3));
+        double d_deltaplus = abs(PR2.getLogval()-PR3.getLogval());
+        lognum MR1(toLogNum(mr1)), MR2(toLogNum(mr2)),MR3(toLogNum(mr3));
+        double d_deltaminus = abs(MR2.getLogval()-MR3.getLogval());
+
+        // take a peak, make sure conversions gone right
+        double refPR1(convertToDouble(PR1)), refPR2(convertToDouble(PR2)),refPR3(convertToDouble(PR3)),
+            refMR1(convertToDouble(MR1)), refMR2(convertToDouble(MR2)), refMR3(convertToDouble(MR3));
+
+        //Dplus tests
+        lognum p_mac_result = PR1;
+        p_mac_result.MAC(PR2,PR3);
+
+        // MSE on multiplication, addition and overall MAC
+        double p_calcMUL = convertToDouble(lognum::multiplyReals(PR2,PR3));
+        double p_refMUL = pr2*pr3;
+        double p_MULdiff = p_refMUL-p_calcMUL;
+        P_MSE_MUL += p_MULdiff*p_MULdiff;
+
+        // this addition is not performed inside of the since this is B+C, but it is a useful comparison
+        double p_acc_ref_diff = (convertToDouble(lognum::addReals(PR2,PR3))-(pr2+pr3));
+        P_MSE_ACC_REF = p_acc_ref_diff * p_acc_ref_diff;
+
+        double p_calcMAC = convertToDouble(p_mac_result);
+        double p_expectedMAC = pr1 + pr2*pr3;
+
+        double p_diff_MAC = (p_calcMAC-p_expectedMAC);
+        P_MSE += p_diff_MAC*p_diff_MAC;
+
+        if (abs(p_diff_MAC) > LARGEFRAC * abs(p_expectedMAC)) {
+            ++P_largeErrorCt;
+        }
+
+        // end Dplus tests
+
+        // Dminus tests
+        lognum m_mac_result = MR1;
+        m_mac_result.MAC(MR2,MR3);
+
+        // see where errors came from
+        double m_calcMUL = convertToDouble(lognum::multiplyReals(MR2,MR3));
+        double m_refMUL = mr2*mr3;
+
+        double m_MULdiff = m_refMUL-m_calcMUL;
+        M_MSE_MUL += m_MULdiff*m_MULdiff;
+
+        double m_acc_ref_diff = (convertToDouble(lognum::addReals(MR2,MR3))-(mr2+mr3));
+        M_MSE_ACC_REF = m_acc_ref_diff * m_acc_ref_diff;
+
+        double m_calcMAC = convertToDouble(m_mac_result);
+        double m_expectedMAC = mr1 + mr2*mr3;
+
+        double m_diff_MAC = (m_calcMAC-m_expectedMAC);
+        M_MSE += m_diff_MAC*m_diff_MAC;
+
+        if (abs(m_diff_MAC) > LARGEFRAC * abs(m_expectedMAC)) {
+            ++M_largeErrorCt;
+        }
+
+        // end Dminus tests
+
+        if (abs(p_acc_ref_diff) > abs(m_acc_ref_diff)) {
+//            printf("Error when the inputs were: b=%f, c=%f\n",(pr2),(pr3));
+            ++unexpectedErrorCt;
+        }
+        printf("\nDelta_Plus d value is: %f",d_deltaplus);
+        printf("\nDelta_Minus d value is : %f\n",d_deltaminus);
+    }
+
+/*
+    printf("Testing specifically on delta plus function - B+C operands must have same sign\n");
+    printMSE_Errors(P_largeErrorCt,P_MSE,P_MSE_ACC_REF,P_MSE_MUL);
+
+    printf("\n\nTesting specifically on delta minus function - B+C operands must have opposite sign\n");
+    printMSE_Errors(M_largeErrorCt,M_MSE,M_MSE_ACC_REF,M_MSE_MUL);
+
+    printf("\n\nUnexpected errors (d+ error > d- error) occured on %2.4f percent of cases",(100.0*unexpectedErrorCt/TESTCASES));
+*/
+
+    cout << "\nExiting a deprecated function.  Stop and find corrected one" << endl;
+}
+
+/*
+ * This functions was created to replace
+ * "runMSETestforDeltaComparison"
+ *
+ * Corresponds to "Corrections_to_Testing_Problem",
+ * which was a fix to the incorrect "Testing_Problem"
+ * */
+
+void runMSE_Delta_Test_Procedure() {
+    double DP_MSE(0.0),DM_MSE(0.0); int DP_bigErrorCt(0),DM_bigErrorCt(0),badErrorCt(0);
+    double DP_totalPer_Error(0.0),DM_totalPer_Error(0.0);
+
+    for (int i = 0; i < TESTCASES; ++i) {
+        int base_num = 1024*32;
+        int32_t r1_int((rand() % (base_num)));
+
+        // 1. Pick a number |a|......OKAY to change a by adding an integer, making it negative, etc. (+=r1_int)
+        double a = (double)rand()/RAND_MAX; //a += r1_int;
+        // 2. Calculate A_fixed (float here)
+        double Afloat = log2(a);
+        // 3. Calculate B_fixed (float here) by moving up to 1 away
+        double eps = (double)rand()/RAND_MAX;
+        double Bfloat = Afloat + eps;
+        // 4. Pick a number |b|
+        double b = pow(2,Bfloat);
+
+        // Exercise all test cases keeping a same for both, splitting b
+        double bplus(b),bminus(b);
+        if (i % 2) {
+            a *= -1.0;
+        }
+
+        if (a<0) {
+           if (bplus>0) {bplus *= -1.0;}
+           if (bminus<0) {bminus *= -1.0;}
+        } else { // a > 0 so make b > 0, bminus < 0
+            if (bplus<0) {bplus *= -1.0;}
+            if (bminus>0) {bminus *= -1.0;}
+        }
+
+        // what we should get
+        double trueDPlus_addn = (a + bplus);
+        double trueDMinus_addn = (a + bminus);
+
+        // move to log domain & compute
+        lognum A(toLogNum(a)),Bplus(toLogNum(bplus)),Bminus(toLogNum(bminus));
+        lognum plusResult = lognum::addReals(A,Bplus);
+        lognum minusResult = lognum::addReals(A,Bminus);
+
+        // what we got
+        double calcDPlus_addn = convertToDouble(plusResult);
+        double calcDMinus_addn = convertToDouble(minusResult);
+
+        // residual
+        double DPlus_diff = abs(trueDPlus_addn - calcDPlus_addn);
+        double DMinus_diff = abs(trueDMinus_addn - calcDMinus_addn);
+
+        // percent error
+        double DPlus_addn_Error_Per = 100.0 * abs(trueDPlus_addn-calcDPlus_addn)/abs(trueDPlus_addn);
+        double DMinus_addn_Error_Per = 100.0 * abs(trueDMinus_addn-calcDMinus_addn)/abs(trueDMinus_addn);
+        if (DPlus_addn_Error_Per > DMinus_addn_Error_Per) {++badErrorCt;}
+        DP_totalPer_Error += DPlus_addn_Error_Per;
+        DM_totalPer_Error += DMinus_addn_Error_Per;
+
+
+        // MSE
+        DP_MSE += (DPlus_diff) * (DPlus_diff);
+        DM_MSE += (DMinus_diff) * (DMinus_diff);
+    }
+    printf("In %d cases, this # had larger delta_minus addition error: %2.6f",TESTCASES,(100.0*(TESTCASES-badErrorCt)/TESTCASES));
+    cout <<"%\n";
+
+    cout << "\nPrinting results of delta plus tests from V2 procedure" << endl;
+//    printMSE_Errors_Short(DP_bigErrorCt,DP_MSE);
+    printf("Avg percent error %2.6f",(DP_totalPer_Error/TESTCASES));
+
+    cout << "%\n\nPrinting results of delta minus tests from V2 procedure" << endl;
+//    printMSE_Errors_Short(DM_bigErrorCt,DM_MSE);
+    printf("Avg percent error %2.6f",(DM_totalPer_Error/TESTCASES));cout <<"%";
+
+}
+
+void printMSE_Errors(int largeErrorCt,double MSE_MAC, double MSE_ACC, double MSE_MUL) {
+    double RMSD_MAC = sqrt(MSE_MAC/TESTCASES);
+    double RMSD_ACC = sqrt(MSE_ACC/TESTCASES);
+    double RMSD_MUL = sqrt(MSE_MUL/TESTCASES);
+    printf("Test cases off by > 10%: \t\t\t %2.6f",((100.0*largeErrorCt)/(TESTCASES))); cout << " %\n";
+    printf("MSE on %d MAC operatons: \t\t\t%.4f",(TESTCASES),(MSE_MAC));
+    printf("\nMSE from reference ADD ops was : \t\t%.4f",(MSE_ACC));
+    printf("\nMSE from the MUL ops was : \t\t\t%.4f",(MSE_MUL));
+
+    printf("\n\nRMSD on %d MAC operations: \t\t%.7f",(TESTCASES), (RMSD_MAC));
+    printf("\nRMSD of ADD ops operations: \t\t\t%.7f",(RMSD_ACC));
+    printf("\nRMSD of MUL operations: \t\t\t%.7f",(RMSD_MUL));
+}
+
+void printMSE_Errors_Short(int largeErrorCt, double MSE) {
+    double RMSD = sqrt(MSE/TESTCASES);
+    printf("Test cases off by > 10%: \t %2.9f",((100.0*largeErrorCt)/(TESTCASES))); cout << " %\n";
+    printf("MSE on %d ops: \t\t%.4f",(TESTCASES),(MSE));
+    printf("\nRMSD on %d ops was: \t\t%.7f",(TESTCASES),(RMSD));
 }
 
 template<typename T>
